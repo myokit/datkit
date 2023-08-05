3a1a65205a6203674ad4d61e3832a4748823b23d
Michael Clerx <michael.clerx@nottingham.ac.uk>
2023-08-05 17:18:53 +0100

    Updated version number

--- datkit/_datkit_version.py
+++ datkit/_datkit_version.py
@@ -11,7 +11,7 @@
 #    incompatibility
 #  - Changes to revision indicate bugfixes, tiny new features
 #  - There is no significance to odd/even numbers
-__version_tuple__ = 0, 0, 2
+__version_tuple__ = 0, 0, 3
 
 # String version of the version number
 __version__ = '.'.join([str(x) for x in __version_tuple__])
